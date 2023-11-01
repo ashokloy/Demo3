9e2abd49cd5ec5f8b2db25b23eeab2defe725778
Ashok B <ashokb@Ashoks-MacBook-Air.local>
2023-11-01 23:34:47 +0530

    first commit

---
+++ src/main/java/com/mylearning/demo/controller/LearningController.java
@@ -0,0 +1,18 @@
+package com.mylearning.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/learn")
+public class LearningController {
+    @Value("${test.value}")
+    String value;
+    @GetMapping("/test")
+    public ResponseEntity<String> getLearning(){
+        return ResponseEntity.ok("Learning Controller"+value);
+    }
+}
