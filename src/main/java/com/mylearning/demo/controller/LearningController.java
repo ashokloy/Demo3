2bed34039c4eb6057ae1a2a062e0be21e2b78736
Ashok B <ashokloy@gmail.com>
2023-12-11 23:10:43 +0530

    add logs, enable mongo, portnumber

--- src/main/java/com/mylearning/demo/controller/LearningController.java
+++ src/main/java/com/mylearning/demo/controller/LearningController.java
@@ -1,18 +1,22 @@
 package com.mylearning.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/learn")
+@Slf4j
 public class LearningController {
     @Value("${test.value}")
     String value;
     @GetMapping("/test")
-    public ResponseEntity<String> getLearning(){
-        return ResponseEntity.ok("Learning Controller"+value);
+    public ResponseEntity<String> getLearning(@RequestParam String param){
+        log.info("this is log {} param {} ", value, param);
+        return ResponseEntity.ok("Learning Controller "+value + " " + param);
     }
 }
